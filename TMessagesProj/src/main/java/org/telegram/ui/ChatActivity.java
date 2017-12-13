73447ad2f4c6a2ab6be80368dc73c9b68c513fa8
CzBiX <gliuwr@gmail.com>
2017-12-13 15:44:57 +0800

    Block his messages even in chat group

--- TMessagesProj/src/main/java/org/telegram/ui/ChatActivity.java
+++ TMessagesProj/src/main/java/org/telegram/ui/ChatActivity.java
@@ -6817,6 +6817,7 @@ public class ChatActivity extends BaseFragment implements NotificationCenter.Not
                     waitingForLoad.remove(index);
                 }
                 ArrayList<MessageObject> messArr = (ArrayList<MessageObject>) args[2];
+                messArr = MessagesController.getInstance(currentAccount).filterBlockedMessages(messArr, false);
                 boolean createUnreadLoading = false;
                 if (waitingForReplyMessageLoad) {
                     if (!createUnreadMessageAfterIdLoading) {
@@ -7403,6 +7404,7 @@ public class ChatActivity extends BaseFragment implements NotificationCenter.Not
                 boolean updateChat = false;
                 boolean hasFromMe = false;
                 ArrayList<MessageObject> arr = (ArrayList<MessageObject>) args[1];
+                arr = MessagesController.getInstance(currentAccount).filterBlockedMessages(arr, false);
                 if (currentEncryptedChat != null && arr.size() == 1) {
                     MessageObject obj = arr.get(0);
 
