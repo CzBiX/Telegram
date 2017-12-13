9d2423161f9d809f979d33c9806be8989d297e7f
CzBiX <gliuwr@gmail.com>
2017-12-13 15:25:51 +0800

    Disable the first camera cell

--- TMessagesProj/src/main/java/org/telegram/ui/Components/ChatAttachAlert.java
+++ TMessagesProj/src/main/java/org/telegram/ui/Components/ChatAttachAlert.java
@@ -2454,7 +2454,8 @@ public class ChatAttachAlert extends BottomSheet implements NotificationCenter.N
     }
 
     public void checkCamera(boolean request) {
-        if (baseFragment == null) {
+        // Disable the first camera cell
+        if (baseFragment == null || true) {
             return;
         }
         boolean old = deviceHasGoodCamera;
